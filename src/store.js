f863c89ba41cdde520f9fb884326658992f3cfaf
AnkuR0124 <ankur.23bcs10185@ms.sst.scaler.com>
2024-06-24 11:13:14 +0530

    redux

--- src/store.js
+++ src/store.js
@@ -2,10 +2,52 @@ import { createStore } from "redux";
 function cartReducer(state= { items: {}}, action) {
   switch (action.type) {
     case "ADD_TO_CART": {
-
+      const product = acttion.payload;
+      if (state.items[product.id]) {
+        return {
+          ...state,
+          items: {
+            ...state.items,
+            [product.id]: {
+              ...state.items[product.id],
+              quantity: state.items[product.id].quantity + 1
+            }
+          }
+        }
+      } else {
+        return {
+          ...state,
+          items: {
+            ...state.items,
+            [product.id]: {
+              ...product,
+              quantity: 1
+            }
+          }
+        }
+      }
     }
     case "REMOVE_FROM_CART": {
+      if(state.items[product.id].quantity <= 1) {
+        // const items = {...state.items};
+        // delete items[product.id];
+        // return {
+        //   ...state,
+        //   items
+  
+      } else {
+        return {
+          ...state,
+          items: {
+            ...state.items,
+            [product.id]: {
+              ...state.items[product.id],
+              quantity: state.items[product.id].quantity - 1
+            }
+          }
+        }
       
+      }
     }
     default:
       return state;
